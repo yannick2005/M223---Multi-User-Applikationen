c7bf8bb7d8f876cf6036b5c47df741b4151c453c
yannick2005 <yannick.schoenhaar@bluewin.ch>
2023-11-16 07:43:13 +0000

    implemented Role endpoints

--- src/main/java/ch/zli/m223/controller/RoleController.java
+++ src/main/java/ch/zli/m223/controller/RoleController.java
@@ -1,5 +1,59 @@
 package ch.zli.m223.controller;
 
+import ch.zli.m223.model.Role;
+import ch.zli.m223.service.RoleService;
+import javax.annotation.security.RolesAllowed;
+import javax.inject.Inject;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
+
+import java.util.List;
+
+@Path("/role")
+@Tag(name = "Role", description = "Handling of roles")
 public class RoleController {
+        @Inject
+        RoleService roleService;
     
+        @GET
+        @Path("/all")
+        @Produces(MediaType.APPLICATION_JSON)
+        @RolesAllowed("admin")
+        public List<Role> getAllRoles() {
+            return roleService.listAll();
+        }
+
+        @GET
+        @Path("/{id}")
+        @Produces(MediaType.APPLICATION_JSON)
+        @RolesAllowed("admin")
+        public Role getRole(@PathParam("id") Long id) {
+            return roleService.findById(id);
+        }
+
+        @POST
+        @Path("/create")
+        @Consumes(MediaType.APPLICATION_JSON)
+        @Produces(MediaType.APPLICATION_JSON)
+        @RolesAllowed("admin")
+        public Role createRole(Role role) {
+            return roleService.create(role);
+        }
+
+        @PUT
+        @Path("/update/{id}")
+        @Consumes(MediaType.APPLICATION_JSON)
+        @Produces(MediaType.APPLICATION_JSON)
+        @RolesAllowed("admin")
+        public Role updateRole(@PathParam("id") Long id, Role role) {
+            return roleService.update(id, role);
+        }
+
+        @DELETE
+        @Path("/delete/{id}")
+        @RolesAllowed("admin")
+        public void deleteRole(@PathParam("id") Long id) {
+            roleService.delete(id);
+        }
 }
--- src/main/java/ch/zli/m223/service/RoleService.java
+++ src/main/java/ch/zli/m223/service/RoleService.java
@@ -1,5 +1,46 @@
 package ch.zli.m223.service;
 
+import ch.zli.m223.model.Role;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+import java.util.List;
+
+@ApplicationScoped
 public class RoleService {
+
+    @Inject
+    EntityManager entityManager;
+
+    @Transactional
+    public List<Role> listAll() {
+        return entityManager.createQuery("FROM Role", Role.class).getResultList();
+    }
+
+    @Transactional
+    public Role findById(Long id) {
+        return entityManager.find(Role.class, id);
+    }
+
+    @Transactional
+    public Role create(Role role) {
+        entityManager.persist(role);
+        return role;
+    }
+
+    @Transactional
+    public Role update(Long id, Role role) {
+        Role entity = entityManager.find(Role.class, id);
+        entity.setRole(role.getRole());
+        return entity;
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        Role entity = entityManager.find(Role.class, id);
+        entityManager.remove(entity);
+    }
     
 }
