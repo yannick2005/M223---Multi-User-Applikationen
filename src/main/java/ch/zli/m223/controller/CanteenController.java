9d5c931c641347cb3359d51430b37241e91f18e2
yannick2005 <yannick.schoenhaar@bluewin.ch>
2023-11-18 13:24:11 +0000

    changed naming of path

--- src/main/java/ch/zli/m223/controller/CanteenController.java
+++ src/main/java/ch/zli/m223/controller/CanteenController.java
@@ -10,7 +10,7 @@ import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import java.util.List;
 
-@Path("/products")
+@Path("/product")
 @Tag(name = "Canteen", description = "Handling of canteens")
 
 public class CanteenController {
